7b9703cff7f6156dcbd8bf47287dcf29471033af
Jimmy <jiimmyschuler@gmail.com>
2024-10-10 11:39:30 -0400

    Lab_04_04

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, today we will be testing if the number 75 is even or odd");
+int input = 75;
+int divider = 2;
+int remainder = 75 % 2;
+        System.out.println("Using the modulus function, we can determine that 75 is odd due to there being a remainder of " + remainder);
+
+
+
+    System.out.println("Hello world!");
+    }
+
+
+
+
+
+}
